c3396f923786a02c6cdc81ff18da0f012886442f
Carlos <cjrl1988@hotmail.com>
2022-04-25 17:35:29 +0200

    Squeleto Spring

---
+++ src/main/java/com/ciber5/calculator21/Calculator21Application.java
@@ -0,0 +1,13 @@
+package com.ciber5.calculator21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber5/calculator21/Calculator21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber5.calculator21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
