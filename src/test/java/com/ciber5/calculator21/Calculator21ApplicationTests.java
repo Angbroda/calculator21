3e23f8a30d63e0d685dc2bf7fa5bafa3a80b3a02
Carlos <cjrl1988@hotmail.com>
2022-04-26 17:50:14 +0200

    Controladores y test

--- src/test/java/com/ciber5/calculator21/Calculator21ApplicationTests.java
+++ src/test/java/com/ciber5/calculator21/Calculator21ApplicationTests.java
@@ -2,12 +2,18 @@ package com.ciber5.calculator21;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class Calculator21ApplicationTests {
 
+  private Calculator calculator = new Calculator();
 	@Test
 	void contextLoads() {
 	}
-
+  
+  @Test
+  public void testSum() {
+    assertEquals(5,calculator.sum(2,3));
+  }
 }
